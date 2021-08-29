6ad4ab3c6711b8facd701d6afdc06067bb756121
badal1906 <badal1906@gmail.com>
2021-08-29 19:28:18 +0530

    added Authprovider for signIn and signOut

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,6 +1,19 @@
+import { useContext } from "react";
+import { auth } from "../firebase";
+
+import { authContext } from "../AuthProvider";
+import { Redirect } from "react-router-dom"
+
 let Home = () => {
+    let user = useContext(authContext);
     return (
+        <>
+        {user ? "" : <Redirect to="/login" />}
         <h1>Home</h1>
+        <button
+        onClick = {() => {auth.signOut();}}
+        >log out</button>
+        </>
     )
 }
 
